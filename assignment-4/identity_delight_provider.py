a5b0dbf746f97fca8d3130889f2653d8a2edf3be
Christian Sassi <21258355+christiansassi@users.noreply.github.com>
2024-04-10 10:40:25 +0200

    updates

--- assignment-4/identity_delight_provider.py
+++ assignment-4/identity_delight_provider.py
@@ -14,7 +14,7 @@ After sending a complaint to my customer, he said he has improved the security o
 
 ## Explaination
 In this challenge, the server offers the option to either generate a username-password pair using RSA or verify an existing one. 
-The RSA algorithm employed employs a 1024-bit key and utilizes the default public exponent of 65537.
+The RSA algorithm employs a 1024-bit key and uses the default public exponent of 65537.
 Upon user request, the server returns the ciphertext of the flag. 
 The initial step involves extracting the 'n' factor used within the current instance of the RSA algorithm. 
 For this task, refer to https://crypto.stackexchange.com/questions/43583/deduce-modulus-n-from-public-exponent-and-encrypted-data
@@ -34,19 +34,19 @@ def get_n(r: pwn.tubes.remote.remote):
 
     # Given that with 2 m, there is a 55% probability that the final n represents the true value, 
     # increasing the number of m to > 3.6 will provide nearly 100% certainty of obtaining the true n.
-    # However, due to encountering "Got EOF while reading in interactive" with pwn (likely due to a buffer overflow issue), this solution only utilizes two values for 'm'. 
+    # However, due to encountering "Got EOF while reading in interactive" with pwn (likely due to a buffer overflow issue), this solution only uses two values for 'm'. 
     # In case of failure, simply restart the script and retry.
     # https://crypto.stackexchange.com/questions/43583/deduce-modulus-n-from-public-exponent-and-encrypted-data
 
-    messages = [] # ((c_i)^2 - c'_i)
+    multiples_of_n = [] # ((c_i)^2 - c'_i)
 
     # Considering potential buffer overflow issues, we need to avoid attempting values that result in incorrect ciphertexts to prevent wasting attempts. 
-    #For instance, 9 gives a ciphertext of 0.
+    # For instance, 9 gives a ciphertext of 0.
     ms = [3] # Keep track of saved messages
 
     m = 1 # Start from 2 (since 1^2 = 1 so it is useless)
 
-    while len(messages) < 2:
+    while len(multiples_of_n) < 2:
         
         m = m + 1
 
@@ -73,10 +73,10 @@ def get_n(r: pwn.tubes.remote.remote):
         ms.extend([m, mp])
 
         # Calculate and save ((c_i)^2 - c'_i) 
-        messages.append(pow(c, 2) - cp)
+        multiples_of_n.append(pow(c, 2) - cp)
     
     # Calculate every combination and then return the n with the highest frequency
-    n = [GCD(combination[0], combination[1]) for combination in list(combinations(messages, 2))]
+    n = [GCD(combination[0], combination[1]) for combination in list(combinations(multiples_of_n, 2))]
     n = Counter(n).most_common(1)[0][0]
 
     return n
@@ -86,7 +86,9 @@ def malleability_attack(r: pwn.tubes.remote.remote, n: int, flag: int):
     # Default e
     e = 65537
 
-    # Just to demonstrate that malleability works with any random g
+    # Just to demonstrate that malleability works with any (more or less) random g
+    # Do not use 1 since it will not generate a new ciphertext
+    # 1000 is just an example, you can modify the upper bound
     g = random.randint(2,1000)
 
     cp = (flag * pow(g, e)) % n
